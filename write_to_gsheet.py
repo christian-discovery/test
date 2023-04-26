4b25065e28e071cf2ae3f8c2fda5fe3f210c64cb
Christian Balondo <88864305+christian-discovery@users.noreply.github.com>
2023-04-25 23:00:19 -0700

    Update write_to_gsheet.py

--- write_to_gsheet.py
+++ write_to_gsheet.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import google.auth
 from googleapiclient.discovery import build
 
@@ -14,9 +15,13 @@ def write_to_gsheet(sheet_id, range_name, values):
     print(f"{result.get('updatedCells')} cells updated.")
 
 def main():
+    action_number = sys.argv[1]
+    action_link = sys.argv[2]
+    action_duration = sys.argv[3]
+
     sheet_id = os.environ['SPREADSHEET_ID']
-    range_name = 'Sheet1!A1'
-    values = [['Hello, World!']]
+    range_name = 'Sheet1!A1'  # Update this to the desired cell range
+    values = [[action_number, action_link, action_duration]]
     write_to_gsheet(sheet_id, range_name, values)
 
 if __name__ == '__main__':
