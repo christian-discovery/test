1ee7154fe265f2b811a585e42222c7250a266792
Christian Balondo <88864305+christian-discovery@users.noreply.github.com>
2023-04-26 01:42:10 -0700

    Update write_to_gsheet.py

--- write_to_gsheet.py
+++ write_to_gsheet.py
@@ -2,11 +2,11 @@ import os
 import google.auth
 from googleapiclient.discovery import build
 
-def write_to_gsheet(sheet_id, range_name, action_number, action_link, action_duration):
+def write_to_gsheet(sheet_id, range_name, platform, device, action_number, action_link, action_duration):
     creds, _ = google.auth.load_credentials_from_file('google_application_credentials.json')
     service = build('sheets', 'v4', credentials=creds)
 
-    values = [[action_number, action_link, action_duration]]
+    values = [[platform, device, action_number, action_link, action_duration]]
     body = {
         'values': values
     }
@@ -21,11 +21,14 @@ def main():
     sheet_id = os.environ['SPREADSHEET_ID']
     range_name = 'Sheet1!A1'  # Update this to the desired cell range
 
+    platform = os.environ['PLATFORM']
+    device = os.environ['DEVICE']
     action_number = os.environ['ACTION_NUMBER']
     action_link = os.environ['ACTION_LINK']
     action_duration = os.environ['ACTION_DURATION']
 
-    write_to_gsheet(sheet_id, range_name, action_number, action_link, action_duration)
+    write_to_gsheet(sheet_id, range_name, platform, device, action_number, action_link, action_duration)
 
-if __name__ == '__main__':
+    if __name__ == '__main__':
     main()
+
