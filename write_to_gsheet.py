6514bf74b55373f1ba525df1bae43ce77c56209e
Christian Balondo <88864305+christian-discovery@users.noreply.github.com>
2023-04-25 23:24:39 -0700

    Update write_to_gsheet.py

--- write_to_gsheet.py
+++ write_to_gsheet.py
@@ -1,29 +1,30 @@
 import os
-import sys
 import google.auth
 from googleapiclient.discovery import build
 
-def write_to_gsheet(sheet_id, range_name, values):
+def write_to_gsheet(sheet_id, range_name, action_number, action_link, action_duration):
     creds, _ = google.auth.load_credentials_from_file('google_application_credentials.json')
     service = build('sheets', 'v4', credentials=creds)
+
+    values = [[action_number, action_link, action_duration]]
     body = {
         'values': values
     }
+
     result = service.spreadsheets().values().update(
         spreadsheetId=sheet_id, range=range_name,
         valueInputOption='RAW', body=body).execute()
+
     print(f"{result.get('updatedCells')} cells updated.")
 
-def main():
-    print("test test: " + sys.argv)
+def main(action_number, action_link, action_duration):
+    sheet_id = os.environ['SPREADSHEET_ID']
+    range_name = 'Sheet1!A1'  # Update this to the desired cell range
+    write_to_gsheet(sheet_id, range_name, action_number, action_link, action_duration)
+
+if __name__ == '__main__':
+    import sys
     action_number = sys.argv[1]
     action_link = sys.argv[2]
     action_duration = sys.argv[3]
-
-    sheet_id = os.environ['SPREADSHEET_ID']
-    range_name = 'Sheet1!A1'  # Update this to the desired cell range
-    values = [[action_number, action_link, action_duration]]
-    write_to_gsheet(sheet_id, range_name, values)
-
-if __name__ == '__main__':
-    main()
+    main(action_number, action_link, action_duration)
